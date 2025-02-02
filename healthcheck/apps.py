5dcf305d3fb12cc51ddde48b47d603452446733f
Antonio Pérez <apema2079@gmail.com>
2025-02-02 06:55:03 -0600

    refactor: add class docstring in healthcheck/apps.py file

--- healthcheck/apps.py
+++ healthcheck/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class HealthcheckConfig(AppConfig):
+    """Health check application configuration."""
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "healthcheck"
